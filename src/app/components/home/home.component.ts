5f5e96939dbb43717554f4c926df1c34975f0194
Javier Leon <dev.javierleon@gmail.com>
2021-04-22 16:09:01 -0500

    seven

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,7 +12,17 @@ export class HomeComponent {
   newSongs: any[] = [];
 
   constructor(private spotify: SpotifyService) {
+    // ------------------------- <> 3
+    // this.spotify.getNewReleases();
 
+    // ------------------------- <> 4
+    // this.spotify.getNewReleases()
+    //   .subscribe( (data: any) => {
+    //     console.log(data.albums.items);
+    //     this.newSongs = data.albums.items;
+    //   });
+
+    // -------------------------- <> 5.6.7
     this.spotify.getNewReleases()
       .subscribe( (data: any) => {
         // console.log(data.albums.items);
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -13,17 +13,45 @@ export class SpotifyService {
     console.log('Here!');
   }
 
-  getNewReleases(): any {
+  getQuery( query: string ): any {
+    const url = `https://api.spotify.com/v1/${ query }`;
     const headers = new HttpHeaders({
-      Authorization: 'Bearer BQAdOrv6bObTFeGH0ASXQWH_C6bkc36INWXrAIodKsVWEwrzqAePyUutfkzE3MzRGxg9c-fkEpfGk5o8PBs'
+      Authorization: 'Bearer BQBaN5K8bou80lNaGyP1h0DPiN77n3kRaqWtxhOUvQNPMm4zQ-aZNqkK2IREbM0YHu9tE3OVCbkj-kUyvYE'
     });
+    return this.http.get(url, { headers });
+  }
 
+  getNewReleases(): any {
+    // ---------------------------------- <> 3
+    // const headers = new HttpHeaders({
+    //   Authorization: 'Bearer BQBNZbpGuPhtwiGm3pbbIeT2FAzXe3PfJT5BLGA7_caNbZ4CDy9qqKCQxs-hyeObqXegL4NfaRPOIqAXdKc'
+    // });
+    //
     // this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers })
     //   .subscribe( data => {
     //     console.log(data);
     //   });
 
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=25&country=kr', { headers })
+    // --------------------------------- <> 4.5
+    // const headers = new HttpHeaders({
+    //   Authorization: 'Bearer BQAdOrv6bObTFeGH0ASXQWH_C6bkc36INWXrAIodKsVWEwrzqAePyUutfkzE3MzRGxg9c-fkEpfGk5o8PBs'
+    // });
+    //
+    // return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=25&country=kr', { headers });
+
+    // --------------------------------- <> 6
+    // const headers = new HttpHeaders({
+    //   Authorization: 'Bearer BQAdOrv6bObTFeGH0ASXQWH_C6bkc36INWXrAIodKsVWEwrzqAePyUutfkzE3MzRGxg9c-fkEpfGk5o8PBs'
+    // });
+    //
+    // return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=25&country=kr', { headers })
+    //   .pipe( map( data => {
+    //     // @ts-ignore
+    //     return data.albums.items;
+    //   }));
+
+    // -------------------------------------------------------------------- <> 7
+    return this.getQuery('browse/new-releases?limit=25&country=kr')
       .pipe( map( data => {
         // @ts-ignore
         return data.albums.items;
@@ -31,11 +59,7 @@ export class SpotifyService {
   }
 
   getArtist(term: string): any{
-    const headers = new HttpHeaders({
-      Authorization: 'Bearer BQAdOrv6bObTFeGH0ASXQWH_C6bkc36INWXrAIodKsVWEwrzqAePyUutfkzE3MzRGxg9c-fkEpfGk5o8PBs'
-    });
-
-    return this.http.get(`https://api.spotify.com/v1/search?q=${encodeURI(term)}&type=artist&limit=25`, { headers })
+    return this.getQuery(`search?q=${encodeURI(term)}&type=artist&limit=25`)
       .pipe( map( data => {
         // @ts-ignore
         return data.artists.items;
